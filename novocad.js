3c0d29cb4d8e2e6f5ee027c7531b51b712b31d6a
jrlxub <jrsiquara@gmail.com>
2024-03-01 14:30:22 -0300

    Adicionando no novocad.js func de mostrar a msg.

--- novocad.js
+++ novocad.js
@@ -2,3 +2,11 @@
 function signUp(email){
     return email;
 }
+
+// pegando o email e cadastrando na plataforma
+
+function mostraMensagem(){
+    return 'Bem vindo ao sistema'
+}
+
+// adicionando funcao de mensagem
